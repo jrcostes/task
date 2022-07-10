61200c4ebd99cb7604acb0f4e8b2985db52e2f91
Jan Rodney Costes <jrmcostes@usep.edu.ph>
2022-07-10 13:42:05 +0800

    add task crud

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,49 @@
+@extends('tasks.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Task</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('tasks.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('tasks.update',$task->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Task Name:</strong>
+                    <input type="text" name="task" value="{{ $task->task }}" class="form-control" placeholder="Task Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Description:</strong>
+                    <textarea class="form-control" style="height:150px" name="description" placeholder="Description">{{ $task->description }}</textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,29 @@
+@extends('tasks.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Task</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('tasks.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Task Name:</strong>
+                {{ $task->task }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Description:</strong>
+                {{ $task->description }}
+            </div>
+        </div>
+    </div>
+@endsection
