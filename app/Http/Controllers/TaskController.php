1cc1a402dc4a3238b914c0ae678e1cdccf025ce4
Jan Rodney Costes <jrmcostes@usep.edu.ph>
2022-07-20 18:52:58 +0800

    more pusher updates

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -58,6 +58,7 @@ class TaskController extends Controller
      */
     public function show(Task $task)
     {
+        $task = Task::paginate(5);
         return view('tasks.show',compact('task'));
     }
 
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -56,6 +56,7 @@
                     @endforeach
                 </tbody>
               </table>
+              {{ $tasks->onEachSide(1)->links() }}
             </div>
           </div>
         </div>
